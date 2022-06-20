5a6439d6ed37f5709c3327b5787a4c7515fad5e2
danilobml <danbarml@gmail.com>
2022-06-20 21:18:18 +0200

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 const { Pool } = require("pg");
 
+const pool = new Pool();
+
+app.get("/api/users", (req, res) => {
+  pool
+    .query("SELECT * FROM users")
+    .then((data) => res.json(data.rows))
+    .catch((error) => res.sendStatus(500));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
